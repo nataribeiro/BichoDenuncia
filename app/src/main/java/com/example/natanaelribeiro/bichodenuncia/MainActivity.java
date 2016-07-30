d787b6c1f93185dd2c655aaae101eab7c40d388a
“Natanael <natanaelribeiro17@gmail.com>
2016-07-30 15:41:46 -0300

    Initial commit

---
+++ app/src/main/java/com/example/natanaelribeiro/bichodenuncia/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.natanaelribeiro.bichodenuncia;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_logo);
+    }
+
+    public void onClickNovaDenuncia(View view) {
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == 1){
+            if(resultCode == RESULT_OK){
+
+            }
+        }
+    }
+}
